65907c9dbca99dd3dd9bcce9734cc4215f23c738
diananov11 <ohlemon0678@gmail.com>
2025-02-21 21:03:37 +0700

    poke test

--- pokemon-test/src/components/PokemonList.jsx
+++ pokemon-test/src/components/PokemonList.jsx
@@ -6,16 +6,26 @@ import PokemonCard from "./PokemonCard";
 
 function PokemonList() {
   // TODO: Tambahkan state untuk menyimpan list pokemon
-  // const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+
+  const handleApi = async () => {
+    let { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+    setPokemons(data.results)
+  }
 
   useEffect(() => {
     // TODO:
     // 1. Gunakan axios.get untuk mengambil data dari:
     //    https://pokeapi.co/api/v2/pokemon?limit=20
     // 2. Simpan data dari response.data.results ke dalam state
+    handleApi()
   }, []);
 
-  return <div className="pokemon-grid">{/* TODO: Tampilkan nama pokemon menggunakan PokemonCard */}</div>;
+  return <div className="pokemon-grid">{/* TODO: Tampilkan nama pokemon menggunakan PokemonCard */}
+    {
+      pokemons.map((item, index) => { return <PokemonCard name={item.name} key={index} /> })
+    }
+  </div>;
 }
 
 export default PokemonList;
